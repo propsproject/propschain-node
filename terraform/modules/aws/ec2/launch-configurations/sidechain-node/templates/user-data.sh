0f0520d890c49eeac4d350cb342b3881e87a93ff
Donald DeWulf <dewulf@younow.com>
2019-06-21 14:02:06 +0300

    fixes

--- terraform/modules/aws/ec2/launch-configurations/sidechain-node/templates/user-data.sh
+++ terraform/modules/aws/ec2/launch-configurations/sidechain-node/templates/user-data.sh
@@ -35,7 +35,7 @@ echo "export SAWTOOTH_REST_URL=${sawtooth_rest_url}" >> /home/ec2-user/.bashrc
 echo "export SAWTOOTH_REST_PORT=${sawtooth_rest_port}" >> /home/ec2-user/.bashrc
 curl http://169.254.169.254/latest/meta-data/public-ipv4 | xargs -I {} -n 1 echo "export PUBLIC_IP_ADDRESS={}" >> /home/ec2-user/.bashrc
 
-croncmd="runuser -l ec2-user -c \"docker-compose -f /opt/sawtooth/docker/docker-compose up -d eth-sync\""
+croncmd="runuser -l ec2-user -c \"docker-compose -f /opt/sawtooth/docker/docker-compose.yaml up -d eth-sync\""
 cronjob="* * * * * $croncmd"
 ( crontab -u ec2-user -l | grep -v "$croncmd" ; echo "$cronjob" ) | crontab -u ec2-user -
 
